b345f7d0275e33e58e3370936a4a53d875538254
Roman Gardukevich <roman@gardukevich.com>
2014-05-30 23:35:50 +0300

    Added pre filled demo credentials.

--- Priorbank Reports/Controllers/RGLoginViewController.m
+++ Priorbank Reports/Controllers/RGLoginViewController.m
@@ -9,6 +9,8 @@
 #import "RGLoginViewController.h"
 #import "RGNetworkManager.h"
 #import "NSString+RGCrypto.h"
+#import "XMLReader.h"
+#import "RGAuthorization.h"
 
 @interface RGLoginViewController ()
 @property(weak, nonatomic) IBOutlet UITextField *loginNameTextField;
@@ -22,6 +24,11 @@
     self.loginNameTextField.placeholder = @"Login name";
     self.passwordTextField.placeholder = @"Your login password";
     [self.signInButton setTitle:@"Sign in" forState:UIControlStateNormal];
+    
+#ifdef DEBUG
+    self.loginNameTextField.text = @"demo";
+    self.passwordTextField.text = @"demo";
+#endif
 }
 
 - (void)viewDidLoad {
@@ -43,7 +50,8 @@
         NSLog(@"ServerToken: %@", serverToken);
 
         [[RGNetworkManager sharedManager] signinWithLoginName:loginName passwordHash:[password sha512] serverToken:serverToken completionBlock:^(NSData *data, NSError *error) {
-
+            RGAuthorization* authorization = [RGAuthorization authorizationWithData:data];
+            
         }];
     }];
 }
