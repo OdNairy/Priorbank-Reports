120695dad77690bc07fafa8f93dc8e3a187fbb65
Roman Gardukevich <roman@gardukevich.com>
2014-05-31 09:19:07 +0300

    Added CardsList Controller

---
+++ Priorbank Reports/Controllers/RGCardsListController.h
@@ -0,0 +1,14 @@
+//
+//  RGCardsListController.h
+//  Priorbank Reports
+//
+//  Created by Roman Gardukevich on 31.05.14.
+//  Copyright (c) 2014 Roman Gardukevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RGCardsList;
+@interface RGCardsListController : UITableViewController
+
+@end
---
+++ Priorbank Reports/Views/Cells/RGCardInListCell.h
@@ -0,0 +1,13 @@
+//
+//  RGCardInListCell.h
+//  Priorbank Reports
+//
+//  Created by Roman Gardukevich on 31.05.14.
+//  Copyright (c) 2014 Roman Gardukevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RGCardInListCell : UITableViewCell
+@property (nonatomic, weak) IBOutlet UILabel* descriptionLabel;
+@end
